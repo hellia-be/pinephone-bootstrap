0b3f3e5184912a3f044008a172b693a1c7024f85
hellia <hellia@kelaun.be>
2021-10-09 11:33:45 +0200

    Setting up thermal throttling

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 sudo pacman -Syyuu
 sudo dd if=/boot/u-boot-sunxi-with-spl-pinephone-552.bin of=/dev/mmcblk2 bs=8k seek=1
 
+echo -e "w /sys/class/thermal/thermal_zone0/trip_point_0_temp - - - - 45000\nw /sys/class/thermal/thermal_zone0/trip_point_1_temp - - - - 75000\nw /sys/class/thermal/thermal_zone0/trip_point_2_temp - - - - 90000" | sudo tee /etc/tmpfiles.d/throttling.conf
+sudo systemd-tmpfiles --create
+
 mkdir -p ~/Documents/git
 
 sudo pacman -S git
