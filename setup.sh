07d1276b1706b9acd29528895d73eb0309cf4d5e
hellia <hellia@kelaun.be>
2021-10-09 12:59:29 +0200

    Correcting yay folder

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ mkdir -p ~/Documents/git
 
 sudo pacman -S git
 cd ~/Documents/git
-git clone https://aur.archlinux.org/yay-bin.git && cd yay
+git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin
 makepkg -si
 
 yay -S puppet
